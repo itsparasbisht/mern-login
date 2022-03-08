c66eeb4df66db056138c41d9edd9ecb8145889e0
parash-sirpi <parash@sirpi.in>
2022-03-08 20:01:20 +0530

    back-end initialized

---
+++ back-end/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/full-stack", (error) => {
+    if (error) {
+        console.log(error)
+    }
+    else {
+        console.log("connected to database")
+    }
+})
+
+const PORT = 5000 || process.env.PORT
+
+app.use(express.json())
+
+app.get('/', function (req, res) {
+    res.json("This is the initial route for our project")
+})
+
+app.use('/api/auth', require('./routes/auth'))
+
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT}`)
+})
