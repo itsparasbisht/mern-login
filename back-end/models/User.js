0b85d9a915b81045db79916432bb78d4cbe9ebd2
parash-sirpi <parash@sirpi.in>
2022-03-10 19:55:47 +0530

    user schema changes

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose")
 const userSchema = mongoose.Schema({
     username: {
         type: String,
+        required: true,
+        unique: true
     },
     password: {
         type: String
     }
 })
 
-module.exports = mongoose.model("users", userSchema)
+module.exports = mongoose.model("Users", userSchema)
