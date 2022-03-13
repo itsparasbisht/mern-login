4ff79f64f4a52fe0ac4c8df89a374faf1ef4e236
parash-sirpi <parash@sirpi.in>
2022-03-13 19:55:34 +0530

    router in app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import SnackbarComponent from './components/SnackbarComponent';
 import LoginPage from './pages/LoginPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import RootPage from './pages/RootPage';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootPage />} />
+          <Route path="/sign-up" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
