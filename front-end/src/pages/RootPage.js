65b16406678081df1b6fc63e2a9cdf432095709c
parash-sirpi <parash@sirpi.in>
2022-03-13 19:55:51 +0530

    root page

---
+++ front-end/src/pages/RootPage.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import React from 'react'
+import { useEffect } from 'react'
+
+axios.defaults.withCredentials = true
+
+function RootPage() {
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    const getUser = async () => {
+        try {
+            const response = await axios.get('/api/auth/get-user')
+            console.log(">>>", response)
+        }
+        catch (error) {
+
+        }
+    }
+    return (
+        <div>RootPage</div>
+    )
+}
+
+export default RootPage
