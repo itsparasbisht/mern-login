a5b62ea78ea06325dfe2ef6f022cc5fbc052f5ac
itsparasbisht <iamparasbisht@gmail.com>
2022-04-03 20:07:06 +0530

    not found style

--- front-end/src/pages/NotFound.js
+++ front-end/src/pages/NotFound.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import ErrorIcon from '@mui/icons-material/Error';
+import './notFound.css'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='notFound__container'>
+      <ErrorIcon className="notFound__container-icon" />
+      <div className='notFound__container-detail'>
+        <h3>404 error</h3>
+        <h5>Sorry, page not found :(</h5>
+      </div>
+    </div>
   )
 }
 
