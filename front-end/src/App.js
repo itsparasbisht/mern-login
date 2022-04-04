49ca49f051ce97cd28c302081e07725ff9480de5
itsparasbisht <iamparasbisht@gmail.com>
2022-04-04 20:22:29 +0530

    protected routes implemented

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,30 +2,29 @@ import './App.css';
 import SignupPage from './pages/SignupPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './pages/HomePage';
-import RootPage from './pages/RootPage';
 import LoginPage from './pages/LoginPage';
-import { useContext } from 'react';
-import userContext from './context/userContext/UserContext';
-import NotFound from './pages/NotFound';
-import AuthGuard from './authGuard/AuthGuard';
+import useAuth from './hooks/useAuth';
 
 function App() {
-  const [userState] = useContext(userContext)
+  
+  const [auth, error] = useAuth()
+  console.log(">>>", auth, error)
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<RootPage />} />
+          
+          <Route path="/" element={auth.isAuthenticated ? <HomePage /> : <LoginPage />} />
 
-          <Route path="/home" element={<AuthGuard />}>
-            <Route path='/home' element={<HomePage />} />
-          </Route>
+          {
+            auth.isAuthenticated && <Route path='/home' element={<HomePage />} />
+          }
 
           <Route path="/log-in" element={<LoginPage />} />
           <Route path="/sign-up" element={<SignupPage />} />
 
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={auth.isAuthenticated ? <HomePage /> : <LoginPage />} />
         </Routes>
       </BrowserRouter>
     </div>
