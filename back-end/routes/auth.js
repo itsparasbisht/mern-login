cd843cdb51b844350b756a174f0b1605b5a63d75
itsparasbisht <iamparasbisht@gmail.com>
2022-05-03 12:24:15 +0530

    log-out api

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,120 +1,143 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const jwt = require('jsonwebtoken');
-const privateKey = "*&ghgds_12WWe&TgFt@1gguDCXXzwE:kjikohi"
+const jwt = require("jsonwebtoken");
+const privateKey = "*&ghgds_12WWe&TgFt@1gguDCXXzwE:kjikohi";
 
 // create a new user
-router.post('/sign-up', async (req, res) => {
-    try {
-        const { username, password } = req.body
+router.post("/sign-up", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-        const user = await User.findOne({ username })
+    const user = await User.findOne({ username });
 
-        if (user) {
-            res.status(200).send({ message: "username exists" })
+    if (user) {
+      res.status(200).send({ message: "username exists" });
+    } else {
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          throw err;
+        } else {
+          const password = hash;
+          const newUser = await User.create({ username, password });
+
+          const payload = {
+            username: newUser.username,
+            id: newUser.id,
+          };
+
+          jwt.sign(
+            payload,
+            privateKey,
+            { algorithm: "HS256" },
+            function (err, token) {
+              if (err) {
+                throw err;
+              }
+              // send the cookie
+              res.cookie("jwt", token, {
+                maxAge: 1000 * 60 * 60 * 24 * 2,
+                httpOnly: true,
+                secure: true,
+              });
+              res
+                .status(201)
+                .send({ username: newUser.username, message: "user created" });
+            }
+          );
         }
-        else {
-            bcrypt.hash(password, saltRounds, async (err, hash) => {
-                if (err) {
-                    throw err
-                }
-                else {
-                    const password = hash
-                    const newUser = await User.create({ username, password })
-
-                    const payload = {
-                        username: newUser.username,
-                        id: newUser.id
-                    }
-
-                    jwt.sign(payload, privateKey, { algorithm: 'HS256' }, function (err, token) {
-                        if (err) {
-                            throw err
-                        }
-                        // send the cookie
-                        res.cookie('jwt', token, { maxAge: 1000 * 60 * 60 * 24 * 2, httpOnly: true });
-                        res.status(201).send({ username: newUser.username, message: "user created" })
-                    });
-                }
-            });
-        }
+      });
     }
-    catch (error) {
-        console.log(">>>", error.message)
-        res.status(500).send({ message: "Internal server error" })
-    }
-})
+  } catch (error) {
+    console.log(">>>", error.message);
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
 
 // get user details
-router.get('/get-user', async (req, res) => {
-    try {
-        const cookies = req.headers.cookie
-        const cookieArray = cookies.split(';')
-        const cookieObj = cookieArray.map(item => {
-            const data = item.split('=')
-            return {
-                "key": data[0],
-                "value": data[1]
-            }
-        })
-        const token = cookieObj[0].value
+router.get("/get-user", async (req, res) => {
+  try {
+    const cookies = req.headers.cookie;
+    const cookieArray = cookies.split(";");
+    const cookieObj = cookieArray.map((item) => {
+      const data = item.split("=");
+      return {
+        key: data[0],
+        value: data[1],
+      };
+    });
+    const token = cookieObj[0].value;
 
-        // verify the token
-        jwt.verify(token, privateKey, async function (err, decoded) {
-            const userId = decoded?.id
+    // verify the token
+    jwt.verify(token, privateKey, async function (err, decoded) {
+      const userId = decoded?.id;
 
-            const user = await User.findById(userId, { password: false })
-            res.status(200).send(user)
-        });
-    }
-    catch (error) {
-        res.status(401).send({ message: "authentication error" })
-    }
-})
+      const user = await User.findById(userId, { password: false });
+      res.status(200).send(user);
+    });
+  } catch (error) {
+    res.status(401).send({ message: "authentication error" });
+  }
+});
 
 // login user
-router.post('/log-in', async (req, res) => {
-    try {
-        const { username, password } = req.body
+router.post("/log-in", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-        const user = await User.findOne({ username })
+    const user = await User.findOne({ username });
 
-        if (user) {
-            const hash = user.password
-            bcrypt.compare(password, hash, function (err, result) {
-                if (result) {
-                    const payload = {
-                        username: user.username,
-                        id: user.id
-                    }
+    if (user) {
+      const hash = user.password;
+      bcrypt.compare(password, hash, function (err, result) {
+        if (result) {
+          const payload = {
+            username: user.username,
+            id: user.id,
+          };
 
-                    jwt.sign(payload, privateKey, { algorithm: 'HS256' }, function (err, token) {
-                        if (err) {
-                            throw err
-                        }
-                        // send the cookie
-                        res.cookie('jwt', token, { maxAge: 1000 * 60 * 60 * 24 * 2, httpOnly: true });
-                        res.send({ username: user.username, message: "user logged in" })
-                    });
-                }
-                else {
-                    res.status(401).send({ message: "invalid credentials" })
-                }
-            });
+          jwt.sign(
+            payload,
+            privateKey,
+            { algorithm: "HS256" },
+            function (err, token) {
+              if (err) {
+                throw err;
+              }
+              // send the cookie
+              res.cookie("jwt", token, {
+                maxAge: 1000 * 60 * 60 * 24 * 2,
+                httpOnly: true,
+                secure: true,
+              });
+              res.send({ username: user.username, message: "user logged in" });
+            }
+          );
+        } else {
+          res.status(401).send({ message: "invalid credentials" });
         }
-        else {
-            res.status(401).send({ message: "user does not exist" })
-        }
+      });
+    } else {
+      res.status(401).send({ message: "user does not exist" });
     }
-    catch (error) {
-        res.status(500).send({ message: "Internal server error" })
-    }
-})
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
 
-module.exports = router
+router.get("/log-out", (req, res) => {
+  try {
+    // delete the cookie
+    res.cookie("jwt", "", { expires: new Date(0) });
+    res.send({ message: "user logged out" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+});
+
+module.exports = router;
