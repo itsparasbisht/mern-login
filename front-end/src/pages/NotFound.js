85ee240288c3fa380b7a904ce6bbc81b25204ce7
itsparasbisht <iamparasbisht@gmail.com>
2022-05-03 12:35:31 +0530

    not found page fixes

--- front-end/src/pages/NotFound.js
+++ front-end/src/pages/NotFound.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import ErrorIcon from '@mui/icons-material/Error';
-import './notFound.css'
+import React from "react";
+import ErrorIcon from "@mui/icons-material/Error";
+import "./notFound.css";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div className='notFound__container'>
+    <div className="notFound__container">
       <ErrorIcon className="notFound__container-icon" />
-      <div className='notFound__container-detail'>
-        <h3>404 error</h3>
-        <h5>Sorry, page not found :(</h5>
+      <div className="notFound__container-detail">
+        <Typography variant="h3">404 error</Typography>
+        <Typography variant="h5">Sorry, page not found :(</Typography>
+        <Link to="/log-in">Back to login</Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
