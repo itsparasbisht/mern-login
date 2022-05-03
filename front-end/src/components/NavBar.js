6f5bf58b7941b1e6957e7eb7fadddc5d87856976
itsparasbisht <iamparasbisht@gmail.com>
2022-05-03 12:24:38 +0530

    implement logout

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,21 +1,66 @@
-import React from 'react'
-import './navBar.css'
-import Button from '@mui/material/Button';
+import React, { useEffect, useState } from "react";
+import "./navBar.css";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import SnackbarComponent from "./SnackbarComponent";
 
 function NavBar() {
-  return (
-    <nav>
-      <div>
-        <img src="/resources/our-logo.webp" alt="our logo" />
-        <span>Foxconn Industries</span>
-      </div>
+  const [username, setUsername] = useState();
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("false");
+  const navigate = useNavigate();
 
-      <p>
-        <span>paras@mail.com</span>
-        <Button variant="contained" color="primary">logout</Button>
-      </p>
-    </nav>
-  )
+  useEffect(() => {
+    setUsername(localStorage.getItem("username"));
+  }, []);
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get("/api/auth/log-out");
+      if (response.data.message === "user logged out") {
+        navigate("/log-in");
+      } else {
+        setError(true);
+        setErrorMessage("Failed to logout");
+      }
+    } catch (error) {
+      setError(true);
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <>
+      <nav>
+        <div>
+          <img src="/resources/our-logo.webp" alt="our logo" />
+          <span>Foxconn Industries</span>
+        </div>
+
+        <p>
+          <Typography variant="body1" component="span">
+            {username}
+          </Typography>
+          <Button
+            color="primary"
+            sx={{ textTransform: "capitalize" }}
+            onClick={handleLogout}
+          >
+            logout
+          </Button>
+        </p>
+      </nav>
+      {error && (
+        <SnackbarComponent
+          handle={true}
+          message={errorMessage}
+          type={"error"}
+        />
+      )}
+    </>
+  );
 }
 
-export default NavBar
+export default NavBar;
