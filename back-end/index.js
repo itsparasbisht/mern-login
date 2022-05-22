291fa2577ecc68c9152300780a14b7e0a0ec1ad6
itsparasbisht <iamparasbisht@gmail.com>
2022-05-22 13:25:14 +0530

    index.js changes

--- back-end/index.js
+++ back-end/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const PORT = 8000 || process.env.PORT;
 
 app.use(cookieParser());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 
-// configure response headers
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// CORS setup
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+// app.use(express.urlencoded({ extended: true }));
 
 // mongoose setup
 const mongoose = require("mongoose");
